6cf4922adaa9e4a031c4db1ca12918bfaacd3db1
ogola <ogolajeremy601@gmail.com>
2024-03-25 22:45:35 +0300

    adds-filter-lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+// const result = drivers.filter(findMatching("Bobby"));
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function matchName(drivers, name){
+    return drivers.filter(match => match.name === name);
+    // for(const item of drivers){
+    //     if (item.name === name){
+    //         return true;
+    //     }`
+    // }
+
+    // return false;
+}
